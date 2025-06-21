4f9734feca46d8dca314c7b65160bd45d98e81a5
Meiguode <xmei6620@gmail.com>
2025-06-21 13:10:36 +0800

    Update annual_warming_extraction.R

--- annual_warming_extraction.R
+++ annual_warming_extraction.R
@@ -1,27 +1,15 @@
-# -----------------------------
-# Load data
-# -----------------------------
 df <- read.csv("regression_matrix.csv")
 
-# -----------------------------
-# Average MAT per year
-# -----------------------------
 library(dplyr)
-
+#-----------------MAT---------------------
 yearly_avg <- df %>%
   group_by(Year) %>%
   summarise(AvgMAT = mean(MAT, na.rm = TRUE)) %>%
   arrange(Year) %>%
   mutate(DiffMAT = c(NA, diff(AvgMAT)))  # year-to-year differences
 
-# -----------------------------
-# Save general year differences
-# -----------------------------
 write.csv(yearly_avg, "yearly_MAT_differences.csv", row.names = FALSE)
 
-# -----------------------------
-# Print only positive differences (warming), rounded to 1 sig fig
-# -----------------------------
 warming <- yearly_avg %>%
   filter(!is.na(DiffMAT) & DiffMAT > 0) %>%
   mutate(Rounded = signif(DiffMAT, digits = 1))
@@ -30,9 +18,6 @@ print("Warming by Year (Rounded to 1 significant figure):")
 print(warming[, c("Year", "Rounded")])
 write.csv(warming, "yearly_MAT_warming.csv", row.names = FALSE)
 
-# -----------------------------
-# Repeat for each Forest.type
-# -----------------------------
 forest_diffs <- df %>%
   group_by(Forest.type, Year) %>%
   summarise(AvgMAT = mean(MAT, na.rm = TRUE), .groups = 'drop') %>%
@@ -41,15 +26,46 @@ forest_diffs <- df %>%
   mutate(DiffMAT = c(NA, diff(AvgMAT))) %>%
   ungroup()
 
-# Save all differences
 write.csv(forest_diffs, "forest_yearly_MAT_differences.csv", row.names = FALSE)
 
-# -----------------------------
-# Print warming years per forest type
-# -----------------------------
 warming_forest <- forest_diffs %>%
   filter(!is.na(DiffMAT) & DiffMAT > 0) %>%
   mutate(Rounded = signif(DiffMAT, digits = 1))
+
+print("Warming by Year per Forest Type (Rounded):")
+print(warming_forest[, c("Forest.type", "Year", "Rounded")])
+write.csv(warming_forest, "yearly_MAT_warming.csv", row.names = FALSE)
+
+#-------------T5-----------------------
+yearly_avg <- df %>%
+  group_by(Year) %>%
+  summarise(AvgT5 = mean(T5, na.rm = TRUE)) %>%
+  arrange(Year) %>%
+  mutate(DiffMAT = c(NA, diff(AvgT5)))  # year-to-year differences
+
+write.csv(yearly_avg, "yearly_T5_differences.csv", row.names = FALSE)
+
+warming <- yearly_avg %>%
+  filter(!is.na(DiffT5) & DiffT5 > 0) %>%
+  mutate(Rounded = signif(DiffT5, digits = 1))
+
+print("Warming by Year (Rounded to 1 significant figure):")
+print(warming[, c("Year", "Rounded")])
+write.csv(warming, "yearly_T5_warming.csv", row.names = FALSE)
+
+forest_diffs <- df %>%
+  group_by(Forest.type, Year) %>%
+  summarise(AvgMAT = mean(T5, na.rm = TRUE), .groups = 'drop') %>%
+  arrange(Forest.type, Year) %>%
+  group_by(Forest.type) %>%
+  mutate(DiffT5 = c(NA, diff(AvgT5))) %>%
+  ungroup()
+
+write.csv(forest_diffs, "forest_yearly_T5_differences.csv", row.names = FALSE)
+
+warming_forest <- forest_diffs %>%
+  filter(!is.na(DiffT5) & DiffT5 > 0) %>%
+  mutate(Rounded = signif(DiffT5, digits = 1))
 
 print("Warming by Year per Forest Type (Rounded):")
 print(warming_forest[, c("Forest.type", "Year", "Rounded")])
